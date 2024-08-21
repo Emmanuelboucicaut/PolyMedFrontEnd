fe8ab8add2789a9fe39d297a9eedf9a38e4d731d
emmanuelboucicaut <emmanuelboucicaut@users.noreply.github.com>
2024-08-21 00:24:57 -0400

    Modify translate service

--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +10,13 @@ import { map } from 'rxjs/operators';
 export class TranslationService {
   private translations: any = {};
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, @Inject(DOCUMENT) private document: Document) {}
 
   loadTranslations(lang: string): Observable<void> {
-    return this.http.get(`./assets/i18n/${lang}.json`).pipe(
+    const baseHref = this.document.getElementsByTagName('base')[0]?.getAttribute('href') || './';
+    const path = `${baseHref}assets/i18n/${lang}.json`;
+
+    return this.http.get(path).pipe(
       map((translations: any) => {
         this.translations = translations;
       })
