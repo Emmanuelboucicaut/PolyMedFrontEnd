f7445253e96081785bb1a8c9b39991fe225caee8
emmanuelboucicaut <emmanuelboucicaut@users.noreply.github.com>
2024-08-20 23:45:12 -0400

    Ajouter docs folder

---
+++ docs/569.1b83b3df81779b9e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkPolyMedFrontEnd=self.webpackChunkPolyMedFrontEnd||[]).push([[569],{569:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
