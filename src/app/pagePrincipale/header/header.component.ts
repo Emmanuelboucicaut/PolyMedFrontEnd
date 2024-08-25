bae15326535d0b04be3c48563d60a1948687ef74
emmanuelboucicaut <emmanuelboucicaut@users.noreply.github.com>
2024-08-24 22:00:25 -0400

    Ajouter nouvelle focntionalite

--- src/app/pagePrincipale/header/header.component.ts
+++ src/app/pagePrincipale/header/header.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { UserSession } from 'src/app/models/user-session';
 import { UserSessionService } from 'src/app/services/user-session.service';
 import { MatSidenav } from '@angular/material/sidenav';
-
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -16,7 +16,8 @@ export class HeaderComponent implements OnInit {
   @Input() sidenav!: MatSidenav;
 
   constructor(
-    private userSession: UserSessionService
+    private userSession: UserSessionService,
+    private authService: AuthService
   ){}
 
   async ngOnInit(): Promise<void> {
@@ -29,5 +30,10 @@ export class HeaderComponent implements OnInit {
 
   toggleSidebar() {
     this.sidenav.toggle();
+  }
+
+  async onLogout() : Promise<void> {
+    this.authService.logout();
+    window.location.reload();
   }
 }
